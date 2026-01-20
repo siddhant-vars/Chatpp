0522fc0e5159d1fa889e924bb202f6ba175b1326
siddhant-vars <siddhantvarshney446@gmail.com>
2026-01-20 13:06:14 +0530

    update package-lock.json and add package.json for project setup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import authroutes from "./routes/auth.route.js"
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 
+const _dirname = path.resolve();
+
 const port = process.env.PORT || 8000
 
 app.use("/api/auth",authroutes)
+
+if(process.env.NODE_ENV == "production") {
+    const frontendpath = path.join(_dirname, "Frontend", "dist");
+
+    app.use(express.static(frontendpath))
+
+    app.get("*",(req, res) => {
+        res.sendFile(path.join(frontendpath, "index.html"))
+    })
+}
+
+
 
 
 app.listen(port, () => {
