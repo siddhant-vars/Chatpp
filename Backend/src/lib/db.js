0a6e53ba5e287d03699a4445950272be0b2784a7
siddhant-vars <siddhantvarshney446@gmail.com>
2026-01-21 23:15:09 +0530

    Refactor process.env → ENV

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { ENV } from "../lib/env.js";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        const conn = await mongoose.connect(ENV.MONGODB_URI)
         console.log("MONGODB connected", conn.connection.host)
     } catch (error) {
         console.log("mongodb connection failed", error)
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,24 +1,23 @@
 import express from "express";
-import dotenv from "dotenv";
 import authroutes from "./routes/auth.route.js"
 import path from "path"
 import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
-dotenv.config();
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const port = process.env.PORT || 8000
+const port = ENV.PORT || 8000
 
 app.use(express.json())
 
 app.use("/api/auth",authroutes)
 
-if(process.env.NODE_ENV == "production") {
+if(ENV.NODE_ENV == "production") {
     const frontendpath = path.join(__dirname, "../../Frontend/dist");
 
     app.use(express.static(frontendpath))
