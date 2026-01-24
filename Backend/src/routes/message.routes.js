f9309ef37d7e918faf20088f56f7c099643d98f4
siddhant-vars <siddhantvarshney446@gmail.com>
2026-01-24 16:02:26 +0530

    refactor message routes to apply middleware globally

--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { getAllContacts, getMessagesbyUserId, sendMessage, getAllChats } from "../controller/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import {arcjetProtection} from "../middleware/arcjet.middleware.js"
 
 const router = express.Router();
+router.use(arcjetProtection, protectRoute)
 
-router.get("/contacts",protectRoute,getAllContacts);
-router.get("/chats", protectRoute,getAllChats);
-router.get("/:id",protectRoute,getMessagesbyUserId);
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/contacts",getAllContacts);
+router.get("/chats", getAllChats);
+router.get("/:id",getMessagesbyUserId);
+router.post("/send/:id", sendMessage);
 
 export default router
