7c67abae11c7dfa86fe4f12a65cb58f767ddc7ce
siddhant-vars <siddhantvarshney446@gmail.com>
2026-01-28 15:37:31 +0530

    signup implemented on frontend

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -4,7 +4,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {arcjetProtection} from "../middleware/arcjet.middleware.js"
 
 const router = express.Router();
-router.use(arcjetProtection)
+//router.use(arcjetProtection)
 
 router.post("/signup", signup)
 router.post("/login", login)
